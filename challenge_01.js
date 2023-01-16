7ed6a6d575b33748ca170999ff201851ab8900d0
git4cc-noob <ktcc.0822@gmail.com>
2023-01-16 10:31:41 +0100

    Summenberechnung

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,8 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+let zahl1 = prompt("Bitte die erste Zahl eingeben: ");
+let zahl2 = prompt("Bitte die Zweite Zahl eingeben:");
+let summe = zahl1 + zahl2;
+
+console.log(summe);
