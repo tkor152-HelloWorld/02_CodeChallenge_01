e36e921d01221738fc04574c551005953344c262
Peter <schmidt.multimedia@gmail.com>
2023-01-16 10:54:02 +0100

    Variante 1

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,29 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+/*********** Variante 1  ****************/
+// Vorteil: gute Lesbarkeit | aber viele Variablen
+
+// Deklaration
+let ziffer1, ziffer2;
+let zahl1, zahl2;
+let summe;
+
+// Eingabe | Wertzuweisung :: Ziffern
+ziffer1 = prompt("Zahl 1: ");
+ziffer2 = prompt("Zahl 2: ");
+console.log(typeof ziffer1);
+console.log(typeof ziffer2);
+
+// Typkonvertierung | Ziffer --> Zahl (string --> number)
+zahl1 = parseInt(ziffer1);
+zahl2 = parseInt(ziffer2);
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+// Berechnung :: Summe / Addition
+summe = zahl1 + zahl2;
+
+// Ausgabe
+console.log("Die Summe der Zahlen ist: " + summe);
